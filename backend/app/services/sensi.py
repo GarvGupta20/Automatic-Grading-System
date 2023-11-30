3b43f2d5a9c61d9b22f182b08629e3eff63e6f3b
Harsh Sharma <77538369+harshsH-28@users.noreply.github.com>
2023-11-30 16:19:23 +0530

    somechanges

--- backend/app/services/sensi.py
+++ backend/app/services/sensi.py
@@ -2,13 +2,9 @@ from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 # Two lists of sentences
-sentences1 = ['The cat sits outside',
-              'A man is playing guitar',
-              'The new movie is awesome']
+sentences1 = ['The First World War created economic and political situation 1. eXPENDITURE: There was a new in India. a huge increase in the defense expenditure which was financed by war loans duties were introduced.. and increasing taxes: custom raised and income tax was introduced.']
 
-sentences2 = ['The dog plays in the garden',
-              'A woman watches TV',
-              'The new movie is so great']
+sentences2 = ['The war created a new economic situation, It led to a huge increase in defence expenditure and increasing taxes, Customs duties were raised, Income tax introduced, Through the war years prices increased-doubling between 1913 and 1918, Leading to extreme hardship for the common people.']
 
 # Compute embedding for both lists
 embeddings1 = model.encode(sentences1, convert_to_tensor=True)
